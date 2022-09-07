b93be84a19eebc3e9e13128fb046d602cba90dcd
razseed <christianmcrae@hotmail.com>
2022-09-07 01:34:52 +0100

    minor edits, work on the telephone button and a revisited headerbar design and layout

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function colorChange(){
     document.getElementById("telephoneCallHandler").style.transitionDuration = '0.5s';
     document.getElementById("telephoneCallHandler").style.backgroundColor= '#7CFC00';
     document.getElementById("telephoneCallHandler").style.color='black';
-    document.getElementById("telephoneCallHandler").style.width = '18vw';
-    document.getElementById("telephoneCallHandler").style.height = '12vh';
+    document.getElementById("telephoneCallHandler").style.width = '17vw';
+    document.getElementById("telephoneCallHandler").style.height = '11vh';
 }
 
 
@@ -15,8 +15,8 @@ function colorChangeRevert(){
     document.getElementById("telephoneCallHandler").style.transitionDuration = '0.5s';
     document.getElementById("telephoneCallHandler").style.backgroundColor= 'rgba(187, 0, 0, 0.871';
     document.getElementById("telephoneCallHandler").style.color= 'white';
-    document.getElementById("telephoneCallHandler").style.width = "";
-    document.getElementById("telephoneCallHandler").style.height = "";
+    document.getElementById("telephoneCallHandler").style.width = '16vw';
+    document.getElementById("telephoneCallHandler").style.height = '10vh';
 }
 // ^^ Controls the button for color changes and sizing. Only below will write script for onclick
 // to handle a phonecall offer. Altenative of  Skype, Teams or Discord.
